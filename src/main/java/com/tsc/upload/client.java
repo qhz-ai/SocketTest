7dcffae17682dac4614dddea31559d3a7fb5c536
qhz <2376484765@qq.com>
2021-04-07 10:54:19 +0800

    更新了client程序

--- src/main/java/com/tsc/upload/client.java
+++ src/main/java/com/tsc/upload/client.java
@@ -10,7 +10,7 @@ public class client {
         //创建流对象，读取本地的配置文件
         BufferedInputStream bis = new BufferedInputStream(new FileInputStream("test.jpg"));
         //创建输出流，写到服务端
-        Socket client = new Socket("127.0.0.1", 7777);
+        Socket client = new Socket("127.0.0.1", 9999);
         BufferedOutputStream bos = new BufferedOutputStream(client.getOutputStream());
         //写出数据
         byte[] bytes = new byte[1024 * 8];
